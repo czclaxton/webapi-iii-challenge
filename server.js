eb01676f3b227747eb0ae90e7444e448a52e1db7
Connor Claxton <czclaxton@gmail.com>
2019-09-04 18:52:23 -0500

    added logger functionality

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
-const express = 'express';
+const express = require("express");
 
 const server = express();
 
-server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`)
+server.use(express.json());
+server.use(logger);
+
+server.get("/", (req, res) => {
+  res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
 //custom middleware
 
 function logger(req, res, next) {
-
-};
+  console.log("Logger: ", req.method, req.url, Date.now());
+  next();
+}
 
 module.exports = server;
